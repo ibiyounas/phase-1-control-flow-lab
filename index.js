45217180f782c7cdfd603d2f53c27605f30747ef
Ibrahem Younas <younasibrahem@gmail.com>
2024-04-01 14:11:35 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ridelength){
+  
+  if (ridelength <= 400) {
+    return 'This one is on me!';
+    
+  }
+  else if (ridelength > 400 && ridelength <=2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (ridelength > 2000 && ridelength <= 2500) {
+  return 'I will gladly take your thirty bucks.'
+  }
+  else {
+  return 'No can do.';
+  }
+  
+}
+
+function ternaryCheckCity(name){
+  return name == 'NYC'? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case tip = 'generous':
+    return "Thank you so much."
+    case tip = 'not as generous':
+    return "Thank you."
+    default:
+      return "Bye."
+  }
 }
